9c0f1d977a8152372291e1940bd415797d45d580
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-02-15 02:25:32 -0300

    feature: Database, Controller created a class for the database created a controller for the User model

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'; // <== sucrase lets this happen
 import routes from './routes'; // without it => needs require(...)
 
+import './database';
+
 class App {
   constructor() {
     this.server = express();
