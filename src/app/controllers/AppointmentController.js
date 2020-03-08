54229fc676b9298042dc0d797c9e7ba284595e05
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-03-08 01:51:37 -0300

    Feature: Appointment Controller Created a controller for Appointments; Created a store function and a post route for it;

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,44 @@
+import * as Yup from 'yup';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Failed' });
+    }
+
+    const { provider_id, date } = req.body;
+    /**
+     * check if provider_id is a real provider
+     */
+
+    const isProvider = await User.findOne({
+      where: {
+        id: provider_id,
+        provider: true,
+      },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Cannot make appointment with non provider users' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      date,
+      provider_id,
+    });
+
+    return res.json({ appointment });
+  }
+}
+
+export default new AppointmentController();
